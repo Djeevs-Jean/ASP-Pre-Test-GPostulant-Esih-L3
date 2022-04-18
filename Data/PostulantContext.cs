39d24da59998290f979a3938b4097c7972a6923a
Djeevs-Jean <jeanyvesbadette@gmail.com>
2022-04-17 21:23:26 -0400

    Initial Data DbContext , and utilitary for install

--- Data/PostulantContext.cs
+++ Data/PostulantContext.cs
@@ -1,7 +1,15 @@
+using GPostulant.Models;
+using Microsoft.EntityFrameworkCore;
+#nullable disable
 namespace GPostulant.Data
 {
-    public class PostulantContext
+    public class PostulantContext : DbContext
     {
         
+        public PostulantContext(DbContextOptions<PostulantContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Postulant> postulants {get; set;}
     }
 }
--- Makefile
+++ Makefile
@@ -3,3 +3,20 @@ run:
 
 build:
 	dotnet build
+
+list_package:
+	dotnet list package
+
+# dotnet remove package <PACKAGE_NAME>
+
+install1:
+	dotnet add package Microsoft.EntityFrameworkCore
+
+install2:
+	dotnet add package Microsoft.EntityFrameworkCore.Design
+
+install3:
+	dotnet add package Microsoft.EntityFrameworkCore.Sqlite
+
+install4:
+	dotnet add package Microsoft.EntityFrameworkCore.tools
