31497ba6462fa7d8271cc4bce0e4080ffeaaa157
Djeevs-Jean <jeanyvesbadette@gmail.com>
2022-04-17 21:28:26 -0400

    Add connection Sqlite Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
+using GPostulant.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+// Add connection Sqlite
+builder.Services.AddDbContext<PostulantContext>(
+    p=>p.UseSqlite(builder.Configuration.GetConnectionString("GPostulantContext"))
+);
 
 var app = builder.Build();
 
